0076d85dfd770251183d259919e60a86b5ac130c
4evajsh@gmail.com <4evajsh@gmail.com>
2021-04-21 11:54:43 +0900

    Spring 프로젝트 생성
    Gradle project
    Lan : java 11
    Spring boot : 2.4.5
    Dependencies :
     - Spring Web
     - Lombok
     - Thymeleaf

---
+++ src/main/java/com/quickorderservice/QuickorderserviceApplication.java
@@ -0,0 +1,13 @@
+package com.quickorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickorderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickorderserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickorderservice/QuickorderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickorderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
