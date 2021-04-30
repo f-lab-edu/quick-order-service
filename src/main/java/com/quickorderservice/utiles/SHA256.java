a957e5ba0b9eb9521662fc625be932af2e6a5de1
setung <4evajsh@gmail.com>
2021-05-01 01:23:17 +0900

    회원 비밀번호 암호화를 위한 SHA256 구현

---
+++ src/main/java/com/quickorderservice/utiles/SHA256.java
@@ -0,0 +1,36 @@
+package com.quickorderservice.utiles;
+
+import lombok.extern.log4j.Log4j;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+    public static String encBySha256(String data) throws Exception {
+        String retVal = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(data.getBytes());
+
+            byte byteData[] = md.digest();
+            StringBuffer sb = new StringBuffer();
+
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                String hex = Integer.toHexString(0xff & byteData[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            retVal = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+        }
+        return retVal;
+    }
+}
