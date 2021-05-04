917b05f68a0e66f09ae7a62c22cf80db62ed64b4
setung <4evajsh@gmail.com>
2021-05-04 21:43:36 +0900

    SHA256 StringBuffer에서 StringBuilder로 교체

--- src/main/java/com/quickorderservice/utiles/SHA256.java
+++ src/main/java/com/quickorderservice/utiles/SHA256.java
@@ -4,6 +4,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SHA256 {
+
     public static String encBySha256(String data) throws Exception {
         String retVal = "";
         try {
@@ -11,13 +12,13 @@ public class SHA256 {
             md.update(data.getBytes());
 
             byte byteData[] = md.digest();
-            StringBuffer sb = new StringBuffer();
+            StringBuilder sb = new StringBuilder();
 
             for (int i = 0; i < byteData.length; i++) {
                 sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
             }
 
-            StringBuffer hexString = new StringBuffer();
+            StringBuilder hexString = new StringBuilder();
             for (int i = 0; i < byteData.length; i++) {
                 String hex = Integer.toHexString(0xff & byteData[i]);
                 if (hex.length() == 1) {
