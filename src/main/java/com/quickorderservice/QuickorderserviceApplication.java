2f62b780730e91dc87b709d96778215966394e0d
setung <4evajsh@gmail.com>
2021-05-07 16:54:33 +0900

    spring profile 롤백
    .gitignore에 application.properties 추가

--- src/main/java/com/quickorderservice/QuickorderserviceApplication.java
+++ src/main/java/com/quickorderservice/QuickorderserviceApplication.java
@@ -7,15 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickorderserviceApplication {
 
 	public static void main(String[] args) {
-		setProperty();
 		SpringApplication.run(QuickorderserviceApplication.class, args);
-	}
-
-	private static void setProperty() {
-		String profile = System.getProperty("spring.profiles.active");
-		if(profile == null) {
-			System.setProperty("spring.profiles.active", "develop");
-		}	
 	}
 
 }
--- src/main/java/com/quickorderservice/profile/ProfileConfig.java
+++
@@ -1,9 +0,0 @@
-package com.quickorderservice.profile;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Import({ ProfileDevelop.class, ProfileProduction.class})
-@Configuration
-public class ProfileConfig {
-}
