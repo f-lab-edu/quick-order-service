8dbfa12100e78c84eb20242197a0e934f7b37d01
setung <4evajsh@gmail.com>
2021-06-30 15:18:51 +0900

    session 정보를 redis에 저장해주는 spring-session-data-redis 추가

--- src/main/java/com/quickorderservice/config/RedisConfig.java
+++ src/main/java/com/quickorderservice/config/RedisConfig.java
@@ -2,23 +2,14 @@ package com.quickorderservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    public LettuceConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
     }
 
 }
--- src/main/java/com/quickorderservice/service/member/MemberLoginService.java
+++ src/main/java/com/quickorderservice/service/member/MemberLoginService.java
@@ -1,7 +1,6 @@
 package com.quickorderservice.service.member;
 
 import com.quickorderservice.exception.auth.NeedLoginException;
-import com.quickorderservice.repository.RedisRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,24 +13,23 @@ public class MemberLoginService {
     private final String MEMBER_ID = "memberID";
     private final MemberService memberService;
     private final HttpSession httpSession;
-    private final RedisRepository redisRepository;
 
     public void login(String userId, String password) {
         memberService.findMemberByIdAndPassword(userId, password);
-        httpSession.setAttribute(MEMBER_ID,userId);
-        redisRepository.set(httpSession.getId(), httpSession);
+        httpSession.setAttribute(MEMBER_ID, userId);
     }
 
     public void logout() {
-        redisRepository.remove(httpSession.getId());
+        httpSession.removeAttribute(MEMBER_ID);
     }
 
     public String getLoginMemberId() {
-        HttpSession session = (HttpSession) redisRepository.get(httpSession.getId());
 
-        if(session==null)
+        String userId = (String) httpSession.getAttribute(MEMBER_ID);
+
+        if (userId == null)
             throw new NeedLoginException("로그인이 필요합니다.");
 
-        return (String) session.getAttribute(MEMBER_ID);
+        return userId;
     }
 }
