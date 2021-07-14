28cdf825b2ce048a91a422b78d14fd48df038366
setung <4evajsh@gmail.com>
2021-07-14 15:03:57 +0900

    enum 값 대문자로 수정

--- src/main/java/com/quickorderservice/enumdata/RestaurantCategory.java
+++ src/main/java/com/quickorderservice/enumdata/RestaurantCategory.java
@@ -1,5 +1,6 @@
 package com.quickorderservice.enumdata;
 
 public enum RestaurantCategory {
-    chicken, korean, chinese, japanese, fastfood, western, etc;
+    CHICKEN, KOREAN, CHINESE, JAPANESE, FASTFOOD, WESTERN, ETC;
 }
+
