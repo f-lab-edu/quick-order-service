2060a873a1f50d51020b61e06ef9dae29cb13943
setung <4evajsh@gmail.com>
2021-07-30 15:14:38 +0900

    Geocoding test 수정

--- src/test/java/com/quickorderservice/utiles/geo/GeocodingTest.java
+++ src/test/java/com/quickorderservice/utiles/geo/GeocodingTest.java
@@ -3,6 +3,7 @@ package com.quickorderservice.utiles.geo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,10 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class GeocodingTest {
 
+    @Autowired Geocoding geocoding;
+
     @Test
     @DisplayName("정상적으로 주소를 입력하면 geo 데이터를 불러온다.")
     public void geo() {
-        GeoData geoDataByAddress = new Geocoding().getGeoDataByAddress("분당구 불정로 1");
+        GeoData geoDataByAddress = geocoding.getGeoDataByAddress("분당구 불정로 1");
         GeoData.Address[] addresses = geoDataByAddress.getAddresses();
 
         Assertions.assertThat(geoDataByAddress.getStatus()).isEqualTo("OK");
@@ -23,7 +26,7 @@ class GeocodingTest {
     @Test
     @DisplayName("잘못된 주소를 입력하면 geo 데이터는 비어있다.")
     public void geoWithWrongAddress() {
-        GeoData geoDataByAddress = new Geocoding().getGeoDataByAddress("잘못된 주소");
+        GeoData geoDataByAddress = geocoding.getGeoDataByAddress("잘못된 주소");
 
         GeoData.Meta meta = geoDataByAddress.getMeta();
         GeoData.Address[] addresses = geoDataByAddress.getAddresses();
