ea93c814788670014c93dfa53e6287aac40d2d48
setung <4evajsh@gmail.com>
2021-08-09 14:41:09 +0900

    CacheConfig에 @AllargusConstructor 제거

--- src/main/java/com/quickorderservice/config/CacheConfig.kt
+++ src/main/java/com/quickorderservice/config/CacheConfig.kt
@@ -1,28 +1,29 @@
 package com.quickorderservice.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import lombok.AllArgsConstructor
 import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-@AllArgsConstructor
 open class CacheConfig(private val connectionFactory: RedisConnectionFactory) {
 
     @Bean
     open fun redisCacheManager(): CacheManager {
         val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer()))
-        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory).cacheDefaults(redisCacheConfiguration).build()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
+            .cacheDefaults(redisCacheConfiguration).build()
     }
 
 }
