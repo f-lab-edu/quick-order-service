b13c5133ca94007ddfd9430e8ff5ab3e6d02bd34
setung <4evajsh@gmail.com>
2021-08-18 00:49:12 +0900

    Redis ConnectionFactiory에 host, port 주입
    외부 redis 서버에 접속하기 위해 필요

--- src/main/java/com/quickorderservice/config/RedisConfig.java
+++ src/main/java/com/quickorderservice/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.quickorderservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -9,9 +10,15 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
